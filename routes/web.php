e3e9aa40e1b7746ce613f19275eb7fac22a812e5
muudzo <tatendawalter62@gmail.com>
2025-04-25 23:04:19 +0200

    main route file implementation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChatbotController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/chatbot', [ChatbotController::class, 'index'])->name('chatbot.index');
+Route::post('/chatbot/message', [ChatbotController::class, 'processMessage'])->name('chatbot.message');
+Route::post('/chatbot/language', [ChatbotController::class, 'switchLanguage'])->name('chatbot.language');
