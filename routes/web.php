63f53867a7e2cd2bd095580ccdce895fb206041e
muudzo <tatendawalter62@gmail.com>
2025-05-01 21:13:43 +0200

    updated web routes to include new endpoint

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use App\Http\Controllers\ChatbotController;
+use Illuminate\Support\Facades\Route;
 
+// Landing page (redirect to chatbot)
+Route::get('/', function () {
+    return redirect()->route('chatbot.index');
+});
+
+// Chatbot routes
 Route::get('/chatbot', [ChatbotController::class, 'index'])->name('chatbot.index');
 Route::post('/chatbot/message', [ChatbotController::class, 'processMessage'])->name('chatbot.message');
 Route::post('/chatbot/language', [ChatbotController::class, 'switchLanguage'])->name('chatbot.language');
+Route::get('/chatbot/phrases', [ChatbotController::class, 'getPhrases'])->name('chatbot.phrases');
